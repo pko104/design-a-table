000f79933c47d452c2a42c3e6c5befceff5f2c8b
pko <peterrko1004@gmail.com>
2020-03-08 18:59:47 -0400

    working vue local file

---
+++ main.js
@@ -0,0 +1,25 @@
+// import { Slide } from 'vue-burger-menu'  // import the CSS transitions you wish to use, in this case we are using `Slide`
+
+// export default {
+//     components: {
+//         Slide // Register your component
+//     }
+// }
+
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app')
+
+
+new Vue(
+    {
+        el: '#app_model',
+        data: {
+            value: "Some string data"
+        }
+    });
